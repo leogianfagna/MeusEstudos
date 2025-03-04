242df11aa99197dca2ecb32fece7b27fe7e4fa37
Léo <142264317+leogianfagna@users.noreply.github.com>
2025-03-04 15:55:18 -0300

    Novas regras e recursão em Prolog

--- exercícios/prolog/intro-1.pl
+++ exercícios/prolog/intro-1.pl
@@ -18,13 +18,21 @@ nasceu(fer,f,edu,duda).
 % Regras
 pai(P,F) :- nasceu(F,_,P,_).
 mae(M,F) :- nasceu(F,_,_,M).
-avoPaterno(A,N) :- nasceu(Alguem,_,A,_),nasceu(N,_,Alguem,_).
-avoMaterno(A,N) :- nasceu(Alguem,_,A,_),nasceu(N,_,_,Alguem).
-avohPaterna(A,N) :- nasceu(Alguem,_,_,A),nasceu(N,_,Alguem,_).
-avohMaterna(A,N) :- nasceu(Alguem,_,_,A),nasceu(N,_,_,Alguem).
-irmaos(X,Y) :- nasceu(X,_,PaiComum,MaeComum),nasceu(Y,_,PaiComum,MaeComum).
+avoPat(A,N) :- nasceu(Pai,m,A,_),nasceu(N,_,Pai,_).
+avoMat(A,N) :- nasceu(Mae,f,A,_),nasceu(N,_,_,Mae).
+avohPat(A,N) :- nasceu(Alguem,_,_,A),nasceu(N,_,Alguem,_).
+avohMat(A,N) :- nasceu(Alguem,_,_,A),nasceu(N,_,_,Alguem).
+irmao(I,X) :- nasceu(I,_,MesmoPai,MesmaMae),nasceu(X,_,MesmoPai,MesmaMae),I\==X. % Para não dizer que eu sou irmão de eu mesmo
+primo(I,X) :- I \== X, avoPat(MesmoAvo,I), avoPat(MesmoAvo,X).
+primo(I,X) :- I \== X, avoMat(MesmoAvo,I), avoMat(MesmoAvo,X).
 
-/* Querys
+progenitor(X,Y) :- pai(X,Y);mae(X,Y).
+ancestral(X, Y) :- progenitor(X, Y).
+ancestral(X, Y) :- progenitor(X, Alguem), ancestral(Alguem, Y).
+
+/*
+Querys:
+  ancestral(Quem, Gui)
   irmaos(gui,joana).
   avohPaterna(duda,gui).
 */
